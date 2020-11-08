29dd87e1a5233ff933068992cca127f466cd9499
RajiNas <60361637+RajiNas@users.noreply.github.com>
2020-11-08 18:18:57 -0500

    activating menu bar
    link all fragments to the proper icons in the menu bar

--- app/src/main/java/com/example/lostandfoundapp/ContainerAccessActivity.java
+++ app/src/main/java/com/example/lostandfoundapp/ContainerAccessActivity.java
@@ -7,9 +7,15 @@ import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 import com.google.firebase.auth.FirebaseAuth;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
@@ -17,10 +23,16 @@ public class ContainerAccessActivity extends AppCompatActivity {
     Button btnSignout;
     private FirebaseAuth mAuth;
 
+    Fragment fr;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_container_access);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
         btnSignout = (Button)findViewById(R.id.buttonSignout);
         mAuth = FirebaseAuth.getInstance();
         btnSignout.setOnClickListener(new View.OnClickListener() {
@@ -31,6 +43,41 @@ public class ContainerAccessActivity extends AppCompatActivity {
                 startActivity(new Intent(ContainerAccessActivity.this, MainActivity.class));
             }
         });
+    }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.messages_item:
+                fr = new MessageFragment();
+                displayFragment();
+                return true;
+            case R.id.profile_item:
+                fr = new ProfilFragment();
+                displayFragment();
+                return true;
+            case R.id.lostList_item:
+                fr = new LostFragment();
+                displayFragment();
+                return true;
+            case R.id.foundList_item:
+                fr = new FoundFragment();
+                displayFragment();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+    public void displayFragment(){
+        FragmentManager manager =getSupportFragmentManager();
+        FragmentTransaction transaction = manager.beginTransaction();
+        transaction.replace(R.id.fragment, fr);
+        transaction.commit();
     }
 }
