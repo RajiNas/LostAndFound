6c797d10d43a3d6e17c7236079967e36bc484b7a
OverTime667 <joel.d99@hotmail.com>
2020-11-25 17:32:49 -0500

    changes made in mapacitivity

--- app/src/main/java/com/example/lostandfoundapp/MapActivity.java
+++ app/src/main/java/com/example/lostandfoundapp/MapActivity.java
@@ -6,9 +6,12 @@ import androidx.fragment.app.FragmentActivity;
 import android.location.Address;
 import android.location.Geocoder;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.SearchView;
 import android.widget.Toast;
 
+import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -18,17 +21,20 @@ import com.google.android.gms.maps.model.MarkerOptions;
 
 import java.io.IOException;
 import java.util.List;
+import java.util.Locale;
 
 public class MapActivity extends FragmentActivity implements OnMapReadyCallback {
     GoogleMap map;
     SupportMapFragment mapFragment;
     SearchView searchView;
+    LatLng latLng;
+    String locationName;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
 
-        String getLocation = getIntent().getStringExtra("address");
+         locationName = getIntent().getStringExtra("address");
 //        Toast.makeText(this, "ddddddddddddddddddddddddd" + getLocation, Toast.LENGTH_SHORT).show();
 
         searchView = findViewById(R.id.sv_location);
@@ -36,39 +42,44 @@ public class MapActivity extends FragmentActivity implements OnMapReadyCallback 
                 .findFragmentById(R.id.google_map);
 
 
+        try {
+            geoLocate();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
 
-        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
-            @Override
-            public boolean onQueryTextSubmit(String query) {
-                searchView.setQuery(getLocation,true);
-
-                String location = searchView.getQuery().toString();
-                List<Address> addressList = null;
-                if(location !=null || !location.equals("")) {
-                    Geocoder geocoder = new Geocoder(MapActivity.this);
-                    try {
-                        addressList = geocoder.getFromLocationName(location,1);
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-                    Address address = addressList.get(0);
-                    LatLng latLng = new LatLng(address.getLatitude(), address.getLongitude());
-                    map.addMarker(new MarkerOptions().position(latLng).title(location));
-                    map.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng,10));
-
-                }
-                return false;
-            }
-            @Override
-            public boolean onQueryTextChange(String s) {
-                return false;
-            }
-        });
         mapFragment.getMapAsync(this);
     }
     @Override
     public void onMapReady(GoogleMap googleMap) {
-        map = googleMap;
+        googleMap.addMarker(new MarkerOptions().position(latLng).title(locationName));
+        googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng,10));
     }
+
+    public void geoLocate( ) throws IOException {
+
+//         locationName = "montreal";
+        Geocoder geocoder = new Geocoder(this);
+        List<Address> addressList = geocoder.getFromLocationName(locationName,1);
+
+         Address add = addressList.get(0);
+        String locality = add.getLocality();
+        Toast.makeText(this, locality, Toast.LENGTH_SHORT).show();
+
+        double lat = add.getLatitude();
+        double longitude = add.getLongitude();
+
+        gotoLocation(lat,longitude);
+
+
+
+    }
+    private void gotoLocation(double lat , double longitude){
+         latLng = new LatLng(lat, longitude);
+        Log.e("MapActivity", "The location is" +latLng.toString());
+      //  map.addMarker(new MarkerOptions().position(latLng));
+        //map.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng,10));
+    }
+
 }
