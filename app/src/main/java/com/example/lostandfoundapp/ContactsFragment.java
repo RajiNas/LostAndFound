9352b198d92cb3c47d21331e5694e929293eb4e6
OverTime667 <joel.d99@hotmail.com>
2020-12-14 00:24:40 -0500

    lot of changes made

--- app/src/main/java/com/example/lostandfoundapp/ContactsFragment.java
+++ app/src/main/java/com/example/lostandfoundapp/ContactsFragment.java
@@ -40,7 +40,7 @@ public class ContactsFragment extends Fragment {
     //    Toolbar toolbar;
     RecyclerView recyclerView;
     ImageView profileIv;
-    TextView nameTv, userStatusTv;
+    TextView nameTv, userStatusTv , contactInfo;
     EditText messageEt;
     ImageButton sendBtn;
 
@@ -74,8 +74,8 @@ public class ContactsFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        View view = inflater.inflate(R.layout.fragment_contacts_og, container, false);
+//        // Inflate the layout for this fragment
+       View view = inflater.inflate(R.layout.fragment_contacts_og, container, false);
 
         recyclerView = (RecyclerView) view.findViewById(R.id.chat_recyclerView);
         profileIv = (ImageView) view.findViewById(R.id.profileIv);
@@ -83,141 +83,144 @@ public class ContactsFragment extends Fragment {
         userStatusTv = (TextView) view.findViewById(R.id.userStatusTv);
         messageEt = (EditText) view.findViewById(R.id.messageEt);
         sendBtn = (ImageButton) view.findViewById(R.id.sendBtn);
-
-        //Layout for recyclerView
-        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getContext());
-        linearLayoutManager.setStackFromEnd(true);
-        //recyclerView properties
-        recyclerView.setHasFixedSize(true);
-        recyclerView.setLayoutManager(linearLayoutManager);
-
-        //firebase auth instance
-        firebaseAuth = FirebaseAuth.getInstance();
-
-        firebaseDatabase = FirebaseDatabase.getInstance();
-        usersDbRef = firebaseDatabase.getReference("Users");
-
-        FirebaseUser user1 = firebaseAuth.getCurrentUser();
-        myUid = user1.getUid();
-
-        // cause of probably error Note <--- I decided to change it from getString extra to a getSharedPrefereneces
-        SharedPreferences sp = getContext().getSharedPreferences("DetailsInfo", Context.MODE_PRIVATE);
-
-        hisUid = sp.getString("hisId", "");//getActivity().getIntent().getStringExtra("hisId");
-        userName = sp.getString("username", "");//getActivity().getIntent().getStringExtra("username");
-        hisImage = sp.getString("image", "");//getActivity().getIntent().getStringExtra("image");
-        smgTitle = sp.getString("ItemTitle","");
-//        Toast.makeText(getContext(), "hisUid: " + hisUid, Toast.LENGTH_SHORT).show();
-//        Toast.makeText(getContext(), "myUid: " + myUid, Toast.LENGTH_SHORT).show();
-
-        //search user to get that user's info
-        Query userQuery = usersDbRef.orderByChild("uid").equalTo(hisUid);
-        nameTv.setText(userName);
-        Picasso.get().load(hisImage).into(profileIv);
-
-        //click button to send message
-        sendBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                //get text from edit text
-                String message = messageEt.getText().toString().trim();
-                //check if text is empty
-                if (TextUtils.isEmpty(message)) {
-                    //text empty
-                    Toast.makeText(getContext(), "Cannot send an empty message...", Toast.LENGTH_SHORT).show();
-                } else {
-                    //text not empty
-                    sendMessage(message);
-                }
-            }
-        });
-        readMessages();
-        seenMessages();
-        Log.e("ContactsFragments"," My uid is : " + myUid);
-        Log.e("ContactsFragments"," his uid is : " + hisUid);
+        contactInfo = view.findViewById(R.id.contact_info);
+//
+//        //Layout for recyclerView
+//        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getContext());
+//        linearLayoutManager.setStackFromEnd(true);
+//        //recyclerView properties
+////       recyclerView.setHasFixedSize(true);
+//    //    recyclerView.setLayoutManager(linearLayoutManager);
+//
+//        //firebase auth instance
+//        firebaseAuth = FirebaseAuth.getInstance();
+//
+//        firebaseDatabase = FirebaseDatabase.getInstance();
+//        usersDbRef = firebaseDatabase.getReference("Users");
+//
+//        FirebaseUser user1 = firebaseAuth.getCurrentUser();
+//        myUid = user1.getUid();
+//
+//        // cause of probably error Note <--- I decided to change it from getString extra to a getSharedPrefereneces
+//        SharedPreferences sp = getContext().getSharedPreferences("DetailsInfo", Context.MODE_PRIVATE);
+//
+//        hisUid = sp.getString("hisId", "");//getActivity().getIntent().getStringExtra("hisId");
+//        userName = sp.getString("username", "");//getActivity().getIntent().getStringExtra("username");
+//        hisImage = sp.getString("image", "");//getActivity().getIntent().getStringExtra("image");
+//        smgTitle = sp.getString("ItemTitle","");
+////        Toast.makeText(getContext(), "hisUid: " + hisUid, Toast.LENGTH_SHORT).show();
+////        Toast.makeText(getContext(), "myUid: " + myUid, Toast.LENGTH_SHORT).show();
+//
+//        //search user to get that user's info
+//        Query userQuery = usersDbRef.orderByChild("uid").equalTo(hisUid);
+//        nameTv.setText(userName);
+//        Picasso.get().load(hisImage).into(profileIv);
+//
+//        //click button to send message
+//        sendBtn.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                //get text from edit text
+//                String message = messageEt.getText().toString().trim();
+//                //check if text is empty
+//                if (TextUtils.isEmpty(message)) {
+//                    //text empty
+//                    Toast.makeText(getContext(), "Cannot send an empty message...", Toast.LENGTH_SHORT).show();
+//                } else {
+//                    //text not empty
+//                    sendMessage(message);
+//                }
+//            }
+//        });
+//        readMessages();
+//        seenMessages();
+//        Log.e("ContactsFragments"," My uid is : " + myUid);
+//        Log.e("ContactsFragments"," his uid is : " + hisUid);
+        String userEmail = getActivity().getIntent().getStringExtra("userEmail");
+        contactInfo.setText(userEmail);
         return view;
     }
 
 
-    private void seenMessages() {
-        userRefForSeen = FirebaseDatabase.getInstance().getReference("Chats");
-        seenListener = userRefForSeen.addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull DataSnapshot snapshot) {
-                for (DataSnapshot ds : snapshot.getChildren()) {
-                    ModelChat chat = ds.getValue(ModelChat.class);
-
-                   // if(chat.getTitleOfMsg().equals(msgTitle) )
-                    if (chat.getReceiver().equals(myUid) && chat.getSender().equals(hisUid)) {
-                        HashMap<String, Object> hasSeenhashMap = new HashMap<>();
-                        hasSeenhashMap.put("isSeen", true);
-                        ds.getRef().updateChildren(hasSeenhashMap);
-                    }
-                }
-            }
-
-            @Override
-            public void onCancelled(@NonNull DatabaseError error) {
-
-            }
-        });
-    }
-
-
-
-    private void readMessages() {
-        chatList = new ArrayList<>();
-        DatabaseReference dbRef = FirebaseDatabase.getInstance().getReference("Chats");
-        dbRef.addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull DataSnapshot snapshot) {
-                chatList.clear();
-                for (DataSnapshot ds : snapshot.getChildren()) {
-                    ModelChat chat = ds.getValue(ModelChat.class);
-                  if ( chat.getReceiver().equals(myUid)  && chat.getSender().equals(hisUid)||
-                            chat.getReceiver().equals(hisUid) && chat.getSender().equals(myUid)) {
-
-                        chatList.add(chat);
-                   }
-                    //adapter
-                    adapterChat = new AdapterChat(getContext(), chatList, hisImage);
-                    adapterChat.notifyDataSetChanged();
-
-                    //set adapter to recyclerView
-                    recyclerView.setAdapter(adapterChat);
-
-                }
-
-            }
-
-            @Override
-            public void onCancelled(@NonNull DatabaseError error) {
-
-            }
-        });
-    }
-
-    private void sendMessage(String message) {
-        String timestamp = String.valueOf(System.currentTimeMillis());
-        DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference();
-        HashMap<String, Object> hashMap = new HashMap<>();
-        hashMap.put("sender", myUid);
-        hashMap.put("receiver", hisUid);
-        hashMap.put("message", message);
-        hashMap.put("timestamp", timestamp);
-        hashMap.put("isSeen", false);
-        hashMap.put("titleOfMsg",smgTitle);
-        databaseReference.child("Chats").push().setValue(hashMap);
-
-        //reset the edittext after sending the message
-        messageEt.setText("");
-    }
+//    private void seenMessages() {
+//        userRefForSeen = FirebaseDatabase.getInstance().getReference("Chats");
+//        seenListener = userRefForSeen.addValueEventListener(new ValueEventListener() {
+//            @Override
+//            public void onDataChange(@NonNull DataSnapshot snapshot) {
+//                for (DataSnapshot ds : snapshot.getChildren()) {
+//                    ModelChat chat = ds.getValue(ModelChat.class);
+//
+//                   // if(chat.getTitleOfMsg().equals(msgTitle) )
+//                    if (chat.getReceiver().equals(myUid) && chat.getSender().equals(hisUid)) {
+//                        HashMap<String, Object> hasSeenhashMap = new HashMap<>();
+//                        hasSeenhashMap.put("isSeen", true);
+//                        ds.getRef().updateChildren(hasSeenhashMap);
+//                    }
+//                }
+//            }
+//
+//            @Override
+//            public void onCancelled(@NonNull DatabaseError error) {
+//
+//            }
+//        });
+//    }
+//
+//
+//
+//    private void readMessages() {
+//        chatList = new ArrayList<>();
+//        DatabaseReference dbRef = FirebaseDatabase.getInstance().getReference("Chats");
+//        dbRef.addValueEventListener(new ValueEventListener() {
+//            @Override
+//            public void onDataChange(@NonNull DataSnapshot snapshot) {
+//                chatList.clear();
+//                for (DataSnapshot ds : snapshot.getChildren()) {
+//                    ModelChat chat = ds.getValue(ModelChat.class);
+//                  if ( chat.getReceiver().equals(myUid)  && chat.getSender().equals(hisUid)||
+//                            chat.getReceiver().equals(hisUid) && chat.getSender().equals(myUid)) {
+//
+//                        chatList.add(chat);
+//                   }
+//                    //adapter
+//                    adapterChat = new AdapterChat(getContext(), chatList, hisImage);
+//                    adapterChat.notifyDataSetChanged();
+//
+//                    //set adapter to recyclerView
+//                    recyclerView.setAdapter(adapterChat);
+//
+//                }
+//
+//            }
+//
+//            @Override
+//            public void onCancelled(@NonNull DatabaseError error) {
+//
+//            }
+//        });
+//    }
+//
+//    private void sendMessage(String message) {
+//        String timestamp = String.valueOf(System.currentTimeMillis());
+//        DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference();
+//        HashMap<String, Object> hashMap = new HashMap<>();
+//        hashMap.put("sender", myUid);
+//        hashMap.put("receiver", hisUid);
+//        hashMap.put("message", message);
+//        hashMap.put("timestamp", timestamp);
+//        hashMap.put("isSeen", false);
+//        hashMap.put("titleOfMsg",smgTitle);
+//        databaseReference.child("Chats").push().setValue(hashMap);
+//
+//        //reset the edittext after sending the message
+//        messageEt.setText("");
+//    }
 
 
     @Override
     public void onPause() {
         super.onPause();
-        userRefForSeen.removeEventListener(seenListener);
+//        userRefForSeen.removeEventListener(seenListener);
     }
 
 }
